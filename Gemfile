133a574bf71fbeee1549a57469934b6fd8f9f4c4
aryanc <aryanc@lambdatest.com>
2025-11-06 16:37:42 +0530

    updated dependencies and made changes as per selenium 4

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
 
-gem 'selenium-webdriver','<= 3.141.59'
-gem 'test-unit', '<= 3.5.3'
-gem 'parseconfig', '~> 1.0', '>= 1.0.8'
+gem 'selenium-webdriver', '~> 4.38.0'
+gem 'test-unit', '~> 3.7.0'
+gem 'parseconfig', '~> 1.1.2'
--- LambdaTest.rb
+++ LambdaTest.rb
@@ -1,75 +1,68 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'test/unit'
-require_relative  "readConf"
+require_relative "readConf"
 
 class AdvanceTest < Test::Unit::TestCase
+  def setup
+    config = ReadConfig.new()
 
-    def setup
-		config = ReadConfig.new()	
-		lt_user = ENV['LT_USERNAME']
-		lt_appkey = ENV['LT_ACCESS_KEY']
-		lt_os = ENV['LT_OPERATING_SYSTEM']
-		lt_browser = ENV['LT_BROWSER']
-		lt_browser_version = ENV['LT_BROWSER_VERSION']
-		if(lt_user == "" || lt_user == nil)
-			lt_user = config.getDetails('LT_USERNAME')
-		end
-		if(lt_appkey == "" || lt_appkey == nil)
-			lt_appkey = config.getDetails('LT_ACCESS_KEY')
-		end
-		if(lt_browser == "" || lt_browser == nil)
-			lt_browser = config.getDetails('LT_BROWSER')
-		end
-		if(lt_os == "" || lt_os ==nil)
-			lt_os = config.getDetails('LT_OPERATING_SYSTEM')
-		end
-		if(lt_browser_version == "" || lt_browser_version == nil)
-			lt_browser_version = config.getDetails('LT_BROWSER_VERSION')
-		end	
-		caps = {						
-			:browserName => lt_browser,			
-			:version => lt_browser_version,			
-			:platform =>  lt_os,
-			:name =>  "RubyRSpecTestSample",
-			:build =>  "LambdaTestSampleApp"
-		} 	
-		puts (caps)
-		@driver = Selenium::WebDriver.for(:remote,
-			:url => "https://"+lt_user+":"+lt_appkey+"@hub.lambdatest.com/wd/hub",
-			:desired_capabilities => caps)
-		
-		@driver.manage.window.maximize
-		
-		@driver.get("https://lambdatest.github.io/sample-todo-app/" )
-	end
+    lt_user = ENV['LT_USERNAME'] || config.getDetails('LT_USERNAME')
+    lt_appkey = ENV['LT_ACCESS_KEY'] || config.getDetails('LT_ACCESS_KEY')
+    lt_os = ENV['LT_OPERATING_SYSTEM'] || config.getDetails('LT_OPERATING_SYSTEM')
+    lt_browser = ENV['LT_BROWSER'] || config.getDetails('LT_BROWSER')
+    lt_browser_version = ENV['LT_BROWSER_VERSION'] || config.getDetails('LT_BROWSER_VERSION')
 
-    def test_Login
-        item_name = "Yey, Lets add it to list"
+    lt_options = {
+      "build" => "LambdaTestSampleApp",
+      "name" => "RubyRSpecTestSample",
+      "platformName" => lt_os,
+      "selenium_version" => "4.38.0",
+      "w3c" => true,
+      "plugin" => "ruby-rspec"
+    }
 
-        #Click on First Checkbox
-        fCheckbox = @driver.find_element(:name, 'li1')
-        fCheckbox.click
-
-        #Click on Second Checkbox
-        sCheckbox = @driver.find_element(:name, 'li2')
-        sCheckbox.click
-    
-        #Enter Item Name 
-        itemNameInput = @driver.find_element(:id, 'sampletodotext')
-        itemNameInput.send_keys item_name
-        
-        #Click on Add Button
-        addButton = @driver.find_element(:id, 'addbutton')
-        addButton.submit
-
-        # Verify Added Item
-        getItemName = @driver.find_element(:xpath, '/html/body/div/div/div/ul/li[6]/span').text
-		assert_equal(getItemName, item_name )
+    # ✅ Use the correct Selenium 4 Options object
+    case lt_browser.downcase
+    when 'chrome'
+      options = Selenium::WebDriver::Options.chrome
+    when 'firefox'
+      options = Selenium::WebDriver::Options.firefox
+    when 'edge'
+      options = Selenium::WebDriver::Options.edge
+    else
+      raise "Unsupported browser: #{lt_browser}"
     end
-    
-    def teardown
-		@driver.quit
-	end
-	
+
+    options.browser_version = lt_browser_version
+    options.add_option('LT:Options', lt_options)
+
+    puts "Starting test with: #{lt_browser} #{lt_browser_version} on #{lt_os}"
+
+    # ✅ Create remote WebDriver (Selenium 4 syntax)
+    @driver = Selenium::WebDriver.for(
+      :remote,
+      url: "https://#{lt_user}:#{lt_appkey}@hub.lambdatest.com/wd/hub",
+      options: options
+    )
+
+    @driver.manage.window.maximize
+    @driver.navigate.to("https://lambdatest.github.io/sample-todo-app/")
+  end
+
+  def test_Login
+    item_name = "Yey, Lets add it to list"
+
+    @driver.find_element(:name, 'li1').click
+    @driver.find_element(:name, 'li2').click
+    @driver.find_element(:id, 'sampletodotext').send_keys(item_name)
+    @driver.find_element(:id, 'addbutton').click
+
+    get_item_name = @driver.find_element(:xpath, '/html/body/div/div/div/ul/li[6]/span').text
+    assert_equal(item_name, get_item_name)
+  end
+
+  def teardown
+    @driver.quit if @driver
+  end
 end
